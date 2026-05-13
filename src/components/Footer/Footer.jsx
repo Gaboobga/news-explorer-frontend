f756ad6d9cf8cad6c9eb7ebf75e54624d1767cee
Gaboobga <gabooobgaa@gmail.com>
2026-05-13 15:02:54 -0600

    feat: add icons to header, footer and favicon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.css';
+import githubIcon from '../../images/Github_icon.svg';
 
 function Footer() {
   return (
@@ -7,9 +8,15 @@ function Footer() {
       <nav className="footer__nav">
         <div className="footer__links">
           <a href="/" className="footer__link">Inicio</a>
-          <a href="https://practicum.com" className="footer__link" target="_blank" rel="noreferrer">Practicum</a>
+          <a href="https://practicum.com" className="footer__link" target="_blank" rel="noreferrer">
+            Practicum
+          </a>
         </div>
-        <div className="footer__icons"></div>
+        <div className="footer__icons">
+          <a href="https://github.com" className="footer__icon-link" target="_blank" rel="noreferrer" aria-label="GitHub">
+            <img className="footer__icon" src={githubIcon} alt="GitHub" />
+          </a>
+        </div>
       </nav>
     </footer>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,52 +1,34 @@
-import { useLocation } from "react-router-dom";
-import "./Navigation.css";
+import { useLocation } from 'react-router-dom';
+import './Navigation.css';
+import loginIconWhite from '../../images/login_icon_white.svg';
+import loginIconBlack from '../../images/login_icon_black.svg';
 
-function Navigation({
-  isLoggedIn,
-  userName,
-  onLoginClick,
-  onSignOut,
-  isLight,
-}) {
+function Navigation({ isLoggedIn, userName, onLoginClick, onSignOut, isLight }) {
   const location = useLocation();
-  const isHome = location.pathname === "/";
-  const isSaved = location.pathname === "/saved-news";
+  const isHome = location.pathname === '/';
+  const isSaved = location.pathname === '/saved-news';
 
   return (
     <nav className="navigation">
       {isLoggedIn ? (
         <div className="navigation__links">
-          <a
-            href="/"
-            className={`navigation__link ${isLight ? "navigation__link_dark" : ""} ${isHome ? (isLight ? "navigation__link_active-dark" : "navigation__link_active") : ""}`}
-          >
+          <a href="/" className={`navigation__link ${isLight ? 'navigation__link_dark' : ''} ${isHome ? (isLight ? 'navigation__link_active-dark' : 'navigation__link_active') : ''}`}>
             Inicio
           </a>
-          <a
-            href="/saved-news"
-            className={`navigation__link ${isLight ? "navigation__link_dark" : ""} ${isSaved ? (isLight ? "navigation__link_active-dark" : "navigation__link_active") : ""}`}
-          >
+          <a href="/saved-news" className={`navigation__link ${isLight ? 'navigation__link_dark' : ''} ${isSaved ? (isLight ? 'navigation__link_active-dark' : 'navigation__link_active') : ''}`}>
             Artículos guardados
           </a>
-          <button
-            className={`navigation__button ${isLight ? "navigation__button_dark" : "navigation__button_logged-in"}`}
-            onClick={onSignOut}
-          >
+          <button className={`navigation__button ${isLight ? 'navigation__button_dark' : 'navigation__button_logged-in'}`} onClick={onSignOut}>
             {userName}
+            <img className="navigation__logout-icon" src={isLight ? loginIconBlack : loginIconWhite} alt="Salir" />
           </button>
         </div>
       ) : (
         <div className="navigation__links">
-          <a
-            href="/"
-            className={`navigation__link ${isLight ? "navigation__link_dark" : ""} ${isHome ? "navigation__link_active" : ""}`}
-          >
+          <a href="/" className={`navigation__link ${isLight ? 'navigation__link_dark' : ''} ${isHome ? (isLight ? 'navigation__link_active-dark' : 'navigation__link_active') : ''}`}>
             Inicio
           </a>
-          <button
-            className={`navigation__button ${isLight ? "navigation__button_dark" : ""}`}
-            onClick={onLoginClick}
-          >
+          <button className={`navigation__button ${isLight ? 'navigation__button_dark' : ''}`} onClick={onLoginClick}>
             Iniciar sesión
           </button>
         </div>
